e42d171429579444965fe39f98cce2167b5e576d
PedroGabriel200 <p3drogabryel@gmail.com>
2025-07-04 01:25:51 -0300

    Def carregar Menu
    Implementei a função carregar Menu

--- src/mapa.py
+++ src/mapa.py
@@ -1,5 +1,5 @@
 import pygame
-from constantes import CAMINHO_CASA, ESCALA_LARGURA, ESCALA_ALTURA, CAMINHO_QUINTAL
+from constantes import CAMINHO_CASA, ESCALA_LARGURA, ESCALA_ALTURA, CAMINHO_QUINTAL, CAMINHO_MENU
 
 def carregar_casa():
     casa_img = pygame.image.load(CAMINHO_CASA).convert()
@@ -9,4 +9,7 @@ def carregar_quintal():
     quintal_img = pygame.image.load(CAMINHO_QUINTAL).convert()
     quintal_img = pygame.transform.scale(quintal_img, (ESCALA_LARGURA, ESCALA_ALTURA))
     return quintal_img
-
+def carregar_Menu():
+    menu_img = pygame.image.load(CAMINHO_MENU).convert()
+    menu_img = pygame.transform.scale(menu_img,(ESCALA_LARGURA, ESCALA_ALTURA))
+    return menu_img
